77a68dc40619cc141550901a2e0775b004bb88fb
konmenel <konmenel@gmail.com>
2024-04-04 14:24:00 +0100

    Add intersection option

--- lasman/_clip.py
+++ lasman/_clip.py
@@ -1,9 +1,14 @@
 #!/bin/env python3
 import argparse
+from math import ceil
+from typing import Callable, Iterable
 import geopandas as gpd
 import laspy
 from shapely.geometry.point import Point
 from alive_progress import alive_bar
+
+
+DEFAULT_CHUNK: int = 100_000
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -26,43 +31,70 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--chunk-size",
         type=int,
-        default=100_000,
-        help="The size of the reading chunk. Default: 100,000 points.",
+        default=DEFAULT_CHUNK,
+        help=f"The size of the reading chunk. Default: {DEFAULT_CHUNK:,} points.",
     )
     parser.add_argument(
         "--external",
         action="store_true",
         help="Clips points outside the polygons in the shapefile.",
     )
+    parser.add_argument(
+        "--intersection",
+        action="store_true",
+        help=(
+            "Clips points that are in the intersection of all polygons "
+            "of the shapefile."
+        ),
+    )
     return parser
 
 
-def _is_inside(polygons: gpd.GeoSeries, x: float, y: float) -> bool:
-    return any((p.contains(Point(x, y)) for p in polygons))
+def _is_inside(polygons: gpd.GeoSeries, x: float, y: float, intersection: bool) -> bool:
+    reduce_func: Callable[[Iterable], bool] = all if intersection else any
+    return reduce_func((p.contains(Point(x, y)) for p in polygons))
 
 
 def _get_contained_list(
     polygons: gpd.GeoSeries,
     points: laspy.lasreader.PointChunkIterator,
-    external: bool
+    external: bool,
+    intersection: bool,
 ) -> list[bool]:
-    return [_is_inside(polygons, x, y) ^ external for x, y in zip(points.x, points.y)]
+    return [
+        _is_inside(polygons, x, y, intersection) ^ external
+        for x, y in zip(points.x, points.y)
+    ]
 
 
-def _write_loop_with_progress_bar(
+def _find_contained_and_write(
+    writer: laspy.LasWriter,
+    polygons: gpd.GeoSeries,
+    external: bool,
+    intersection: bool,
+    bar,  # Alive bar instance
+    points,
+) -> None:
+    contained = _get_contained_list(polygons, points, external, intersection)
+    writer.write_points(points[contained])
+    bar()
+
+
+def _main_loop_with_progress_bar(
     writer: laspy.LasWriter,
     reader: laspy.LasReader,
     polygons: gpd.GeoSeries,
     chunk_size: int,
     external: bool,
+    intersection: bool,
 ) -> None:
     npoints: int = reader.header.point_count
     monitor_str = "{count}k/{total}k points done [{percent:.1f}%]"
-    with alive_bar(npoints // chunk_size, monitor=monitor_str) as bar:
+    with alive_bar(ceil(npoints / chunk_size), monitor=monitor_str) as bar:
         for points in reader.chunk_iterator(chunk_size):
-            contained = _get_contained_list(polygons, points, external)
-            writer.write_points(points[contained])
-            bar()
+            _find_contained_and_write(
+                writer, polygons, external, intersection, bar, points
+            )
 
 
 def main(args=None) -> int:
@@ -75,7 +107,14 @@ def main(args=None) -> int:
 
     with laspy.open(args.input) as reader:
         with laspy.open(args.output, mode="w", header=reader.header) as writer:
-            _write_loop_with_progress_bar(writer, reader, polygons, args.chunk_size, args.external)
+            _main_loop_with_progress_bar(
+                writer,
+                reader,
+                polygons,
+                args.chunk_size,
+                args.external,
+                args.intersection,
+            )
 
     return 0
 
