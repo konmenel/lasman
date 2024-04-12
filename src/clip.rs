b2fa18aa77920b3c43b3d47695edbac2fe25c57c
konmenel <konmenel@gmail.com>
2024-04-12 11:53:02 +0100

    Minor changes

--- src/clip.rs
+++ src/clip.rs
@@ -1,6 +1,8 @@
+pub extern crate las;
+pub extern crate shapefile;
 use anyhow::{Context, Ok, Result};
 use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
-use las::{Header, Read, Reader, Write, Writer};
+use las::{Header, Point as lasPoint, Read, Reader, Write, Writer};
 use num_format::{Locale, ToFormattedString};
 use rayon::prelude::*;
 use shapefile::record::polygon::GenericPolygon;
@@ -27,7 +29,7 @@ impl fmt::Display for Strategy {
     }
 }
 
-fn polymz2poly<PointType>(polym: &GenericPolygon<PointType>) -> Polygon
+pub fn polymz2poly<PointType>(polym: &GenericPolygon<PointType>) -> Polygon
 where
     PointType: ShrinkablePoint + GrowablePoint + PartialEq + HasXY + Copy,
 {
@@ -46,28 +48,20 @@ where
     Polygon::with_rings(poly_rings)
 }
 
-fn transform_point(point: &mut Point, scales: &[f64; 2], offsets: &[f64; 2]) {
+fn transform_point(point: &mut Point, offsets: &[f64; 2]) {
     point.x -= offsets[0];
     point.y -= offsets[1];
-    point.x /= scales[0];
-    point.y /= scales[1];
 }
 
-fn winding_number(
-    point: &las::Point,
-    polygon: &Polygon,
-    scales: &[f64; 2],
-    offsets: &[f64; 2],
-) -> i32 {
+pub fn winding_number(point: &Point, polygon: &Polygon, offsets: &[f64; 2]) -> i32 {
     let mut wn = 0;
-    let mut point = Point::new(point.x, point.y);
-    transform_point(&mut point, scales, offsets);
+    let mut point: Point = Point::new(point.x, point.y);
+    transform_point(&mut point, offsets);
     for window in polygon.rings()[0].points().windows(2) {
-        let mut p1 = window.first().unwrap().clone();
-        // let mut p1 = Point::new(p1.x, p1.y);
-        transform_point(&mut p1, scales, offsets);
-        let mut p2 = window.last().unwrap().clone();
-        transform_point(&mut p2, scales, offsets);
+        let mut p1: Point = window.first().unwrap().clone();
+        transform_point(&mut p1, offsets);
+        let mut p2: Point = window.last().unwrap().clone();
+        transform_point(&mut p2, offsets);
 
         if point.x > p1.x && point.x > p2.x {
             continue;
@@ -95,13 +89,8 @@ fn winding_number(
     wn
 }
 
-fn is_pnt_in_poly(
-    point: &las::Point,
-    polygon: &Polygon,
-    scales: &[f64; 2],
-    offsets: &[f64; 2],
-) -> bool {
-    winding_number(point, polygon, scales, offsets) != 0
+pub fn is_point_in_poly(point: &Point, polygon: &Polygon, offsets: &[f64; 2]) -> bool {
+    winding_number(point, polygon, offsets) != 0
 }
 
 fn load_polygons<P: AsRef<Path>>(shapefile: P) -> Result<Vec<Polygon>> {
@@ -126,28 +115,16 @@ fn load_polygons<P: AsRef<Path>>(shapefile: P) -> Result<Vec<Polygon>> {
 fn filter_fn(
     strategy: Strategy,
     polygons: &Vec<Polygon>,
-    point: &las::Point,
+    point: &lasPoint,
     external: bool,
-    scales: &[f64; 2],
     offsets: &[f64; 2],
 ) -> bool {
-    let op = |poly| is_pnt_in_poly(point, poly, scales, offsets) != external;
+    let point = Point::new(point.x, point.y);
+    let op = |poly| is_point_in_poly(&point, poly, offsets) != external;
     match strategy {
         Strategy::Union => polygons.iter().any(op),
         Strategy::Intersection => polygons.iter().all(op),
     }
-}
-
-fn get_scale_and_offsets(header: &Header) -> Result<[[f64; 2]; 2]> {
-    let raw_header = header
-        .clone()
-        .into_raw()
-        .with_context(|| format!("Error converting las header into raw las header"))?;
-
-    Ok([
-        [raw_header.x_scale_factor, raw_header.y_scale_factor],
-        [raw_header.x_offset, raw_header.y_offset],
-    ])
 }
 
 fn print_info<P: AsRef<Path> + std::fmt::Display>(
@@ -223,8 +200,7 @@ pub fn clip<P: AsRef<Path> + std::fmt::Display>(
         println!("Output file \"{}\" already exists.", outfile);
         let mut user_inpur = String::new();
 
-        while !["y", "yes", "n", "no"].contains(&user_inpur.trim().to_lowercase().as_str())
-        {
+        while !["y", "yes", "n", "no"].contains(&user_inpur.trim().to_lowercase().as_str()) {
             user_inpur.clear();
             print!("Overwrite [y/n]: ");
             io::stdout().flush()?;
@@ -249,26 +225,30 @@ pub fn clip<P: AsRef<Path> + std::fmt::Display>(
     // Open input and output las files
     let mut reader = Reader::from_path(lasfile.as_ref())
         .with_context(|| format!("Cannot open las file \"{lasfile}\""))?;
-    let out_header = reader.header().clone();
+    let out_header: Header = reader.header().clone();
     let mut writer = Writer::from_path(outfile.as_ref(), out_header)
         .with_context(|| format!("Cannot open las output file \"{outfile}\""))?;
 
     // Prepare progress bar
-    let total = (reader.header().number_of_points() as f64 / 1000.0).ceil() as u64;
-    let pb = create_progress_bar(total)?;
+    let total: u64 = (reader.header().number_of_points() as f64 / 1000.0).ceil() as u64;
+    let pb: ProgressBar = create_progress_bar(total)?;
 
     // Main loop
     let started = Instant::now();
 
     println!("[2/2] Clipping points");
-    let points_total = reader.header().number_of_points();
-    let [scales, offsets] = get_scale_and_offsets(reader.header())?;
+    let points_total: u64 = reader.header().number_of_points();
+    // let [scales, offsets] = get_scale_and_offsets(reader.header())?;
+    let offsets: [f64; 2] = [
+        reader.header().bounds().min.x,
+        reader.header().bounds().min.x,
+    ];
     let mut points_processes = 0;
     while points_processes < points_total {
         let points = reader.read_n(read_chunk.min(points_total - points_processes))?;
         let contained: Vec<&las::Point> = points
             .par_iter()
-            .filter(|&pnt| filter_fn(strategy, &polygons, pnt, external, &scales, &offsets))
+            .filter(|&pnt| filter_fn(strategy, &polygons, pnt, external, &offsets))
             .collect();
 
         for &p in contained.iter() {
